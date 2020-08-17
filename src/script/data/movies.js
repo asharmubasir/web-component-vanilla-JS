8de0dda74015348ee96ba9ad3d76de0be89ad158
Ashar Mubasir <asharmubasir@gmail.com>
2020-08-17 18:15:51 +0700

    Add loading

--- src/script/data/movies.js
+++ src/script/data/movies.js
@@ -4,12 +4,16 @@ const apiKey = '429906073710e8cd51e02eac25a48caa'
 class DataMovies {
 
   static getAllMovies(page) {
+    
+    document.querySelector('.sk-cube-grid').classList.remove('hidden')
+
     return fetch(`${baseUrl}/top_rated?api_key=${apiKey}&language=en-US&page=${page}`)
     .then(response => {
       return response.json()
     })
     .then(responseJSON => {
       if(responseJSON.results) {
+        document.querySelector('.sk-cube-grid').classList.add('hidden')
         return Promise.resolve(responseJSON);
       } else {
         return Promise.reject('Something wrong');
